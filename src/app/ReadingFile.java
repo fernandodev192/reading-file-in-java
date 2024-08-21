ff38cf80296fa2d494fbb9b8f68683d0a5387d32
fernandodev192 <fernando.dev192@gmail.com>
2024-08-21 12:33:21 -0300

    added enter and header functions

--- src/app/ReadingFile.java
+++ src/app/ReadingFile.java
@@ -1,20 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package app;
 
-/**
- *
- * @author fernando
- */
-public class ReadingFile {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+
+public class ReadingFile {
+    
+    static String enter(String str, Scanner sc) {
+        System.out.print(str);
+        return sc.nextLine();
+    }
+    
+    static void header(String product, String price, String quantity) {
+        System.out.println("------------------------------------------------------------------------");
+        System.out.printf("%-30s %20s %20s\n", product, price, quantity);
+        System.out.println("------------------------------------------------------------------------");
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        try (Scanner sc = new Scanner(System.in)) {
+            
+            String sourceFileStr = enter("Enter the file path: ", sc);
+            
+            
+        }
+        
     }
     
 }
